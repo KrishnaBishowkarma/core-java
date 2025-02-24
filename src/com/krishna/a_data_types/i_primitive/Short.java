d768bea8d8831e7f886aba1a8447bd32e66ca631
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-02-24 10:41:54 +0545

    feat(data-types): add example for short primitive type

---
+++ src/com/krishna/a_data_types/i_primitive/Short.java
@@ -0,0 +1,13 @@
+package com.krishna.a_data_types.i_primitive;
+
+public class Short {
+    public static void main(String[] args){
+        // The short data type can store a whole number between -32,768 and 32,767
+        short loan = -10000;
+        short money = 32767;
+
+        int total = money -loan;
+
+        System.out.println("Total : " + total);
+    }
+}
