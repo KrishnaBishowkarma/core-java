7f521d1de32a5db35338ee6d22b740f5969ba750
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-02-24 11:45:58 +0545

    feat(data-types): add example for char data type storing a character

---
+++ src/com/krishna/a_data_types/i_primitive/Char.java
@@ -0,0 +1,10 @@
+package com.krishna.a_data_types.i_primitive;
+
+public class Char {
+    public static void main(String[] args) {
+        // A character data type, also known as char, is used to store a single character, such as a letter, number, or symbol
+        char grade = 'A';
+
+        System.out.println("The grade is: " + grade);
+    }
+}
