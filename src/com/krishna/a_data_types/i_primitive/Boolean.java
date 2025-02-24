f6cd71c66d2e6c4b2f3267a1c165fda86379e68d
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-02-24 11:53:38 +0545

    feat(data-types): add example for boolean data type with conditional check

---
+++ src/com/krishna/a_data_types/i_primitive/Boolean.java
@@ -0,0 +1,20 @@
+package com.krishna.a_data_types.i_primitive;
+
+public class Boolean {
+    public static void main(String[] args) {
+        // The boolean data type is used to store a true or false value
+        boolean isStudent = true;
+        boolean isTeacher = false;
+
+        if (isStudent) {
+            System.out.println("The person is a Student!");
+        } else {
+            System.out.println("The person is not a Student!");
+        }
+
+        if (isTeacher) {
+            System.out.println("The person is a Teacher");
+        } else
+            System.out.println("The person is not a Teacher");
+    }
+}
